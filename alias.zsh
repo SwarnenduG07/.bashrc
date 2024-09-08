7b5a3c2f81721a8ffff4a71dc3a25602b83d7c19
Sunny <swarnendughosh05@gmail.com>
2024-09-09 00:55:30 +0530

    more alias added

--- alias.zsh
+++ alias.zsh
@@ -11,11 +11,12 @@ alias ga='git add'
 alias gb='git branch'
 alias gbd='git branch -D'
 alias gcb='git checkout -b'
-alias gb='git checkout'
+alias gch='git checkout'
 alias pull='git pull'
-alias gr='git restore --stage'
+alias grs='git restore --stage'
 alias gcl='git clone'
 alias gr='git remote add origin'
+alias gv='git remote -v'
 
 # Custom Docker aliases
 alias d='docker'
@@ -29,13 +30,21 @@ alias c.='cd ..'
 alias c..='cd ../../'
 alias c...='cd ../../../'
 alias c....='cd ../../../../'
+alias dc='cd /d/coding/'
 
 # Custom WebDev aliases
 alias tailwindinit='npm install -D tailwindcss postcss autoprefixer && npx tailwindcss init -p'
 alias nextinit='npx create-next-app@latest'
+alias shadcninit='npx shadcn@latest init'
+alias sadadd='npx shadcn@latest add'
+alias build='npm run build'
+alias dev='npm run dev'
+alias ydev='yarn dev'
 
 
 
 # Custom VsCode aliases
 alias c.='code .'
+
+
 
